3b2dceb67a1c2c5b2a722dfdf320a076b834a899
unknown <raulheri43@hotmail.com>
2022-07-18 00:49:22 -0300

    correção no layout

--- components/section01/Section01.js
+++ components/section01/Section01.js
@@ -6,6 +6,9 @@ const Section01 = () => {
     return (
         <section className={styles.section01}>
             <div className={styles.section01Content}>
+                {/* <svg className={styles.svgScreen} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#0c0f5e" fillOpacity="1" d="M0,0L1440,192L1440,320L0,320Z"></path>
+                </svg> */}
                 <div className={styles.section01h1}>
                     <h1 className={styles.txtH1}>Mais que uma academia, somos
                         <br />uma fábrica de CAMPEÕES!
@@ -16,14 +19,14 @@ const Section01 = () => {
                     </div>
                 </div>
 
-
-                <Image
-                    src={'/gymBackGround.png'}
-                    layout={'fill'}
-                    className={styles.gymBg}
-                    priority
-                />
-
+                <div className={styles.gymBackGround}>
+                    <Image
+                        src={'/gymBackGround.png'}
+                        layout={'fill'}
+                        className={styles.gymBg}
+                        priority
+                    />
+                </div>
             </div>
         </section>
     )
