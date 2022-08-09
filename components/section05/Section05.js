8c0e3c55214592138466b7b2dc57bc1e9175e213
Raul Henrique <raulheri43@hotmail.com>
2022-08-09 12:06:46 -0300

    mudanças no layout

--- components/section05/Section05.js
+++ components/section05/Section05.js
@@ -29,14 +29,13 @@ const Section05 = () => {
         <div className={styles.mapouter}>
           <div className={styles.gmap_canvas}>
             <iframe
-              title="Localização da academia"
               className={styles.gmap_iframe}
-              width="100%"
-              frameBorder="0"
-              scrolling="yes"
-              marginHeight="0"
-              marginWidth="0"
-              src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Academia Top Treino - Rua Sete de Setembro - Cavaleiro, Jaboatão dos Guararapes - PE&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1975.0509481250754!2d-34.97016542135604!3d-8.091090568341295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sAcademia!5e0!3m2!1spt-BR!2sbr!4v1659890755493!5m2!1spt-BR!2sbr"
+              width="300"
+              height="450"
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
         </div>
